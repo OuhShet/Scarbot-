2adf4af00830da95636fff2f206c73bec8cd2555
OuhShet <kml.zapata@gmail.com>
2019-11-20 12:47:14 -0300

    faltan movimientos del scarbot

--- Scarbotv2/Form1.Designer.cs
+++ Scarbotv2/Form1.Designer.cs
@@ -29,11 +29,199 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(631, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Desconectar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(75, 560);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Enviar ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(631, 574);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Guardar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4"});
+            this.comboBox1.Location = new System.Drawing.Point(398, 458);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(398, 505);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Desconectado";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(56, 415);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(270, 139);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(606, 415);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(261, 139);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Datos Enviados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(690, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Datos Recibidos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 317);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(645, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(308, 317);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(377, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(224, 317);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 620);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- Scarbotv2/Form1.cs
+++ Scarbotv2/Form1.cs
@@ -7,14 +7,193 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace Scarbotv2
 {
     public partial class Form1 : Form
     {
+        String mensaje;
+        VideoCapture _capture;
+        private Mat _frame;
+
         public Form1()
         {
             InitializeComponent();
+
+            _capture = new VideoCapture(0);
+            _capture.ImageGrabbed += ProcessFrame;
+            _frame = new Mat();
+
+            if(_capture != null)
+            {
+
+                try
+                {
+                    _capture.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        private void ProcessFrame(Object sender, EventArgs e)
+        {
+
+            if( _capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+                Image<Bgr, Byte> image = _frame.ToImage<Bgr, byte> ();// toma foto
+                image.Save("capture3.jpg");
+                pictureBox1.Image = _frame.Bitmap;
+
+           
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.SelectedIndex = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(serialPort1.IsOpen == false)
+            {
+                serialPort1.BaudRate = 9600;
+                serialPort1.DataBits = 8;
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                serialPort1.Open();
+                textBox1.Text = "Conectado";
+
+
+                MessageBox.Show("Puerto Conectado");
+                textBox1.BackColor = Color.Lime;
+                timer1.Enabled = true;
+
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if( serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+                timer1.Enabled = false;
+                textBox1.Text = "Desconectado";
+
+                MessageBox.Show("Puerto Desconectado");
+                textBox1.BackColor = Color.Red;
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(richTextBox1.Text);
+            serialPort1.Write("\r");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mensaje = serialPort1.ReadExisting();
+
+            if(mensaje.Length>0)
+            {
+
+             /*
+             * Aca se reciben los mensajes del robot y tienen que realizar la captura y procesamiento de las imagenes
+             */
+               
+            if( mensaje.Contains("OK2"))  //verifica tabla es afirmativa
+                {
+                    serialPort1.Write("run 1"); /// tomar eje A
+                    serialPort1.Write("\r");
+                }
+            if(mensaje.Contains("OK3"))
+                {
+                    serialPort1.Write("run 2");
+                    serialPort1.Write("\r");
+
+                }
+
+            if(mensaje.Contains("OK1"))
+                {
+                    Image<Bgr, byte> image = _frame.ToImage<Bgr, byte>();
+                    Boolean flag = false;
+                    //Image<Bgr, byte> Mostrar = Image.Copy();
+
+                    for(int i = 1; i < 13; i++)
+                    {
+                        Image<Bgr, byte> Patron = new Image<Bgr, byte>("" + i + ".jpg"); // imagen del patron
+
+                        using (Image<Gray, float> resultado = image.MatchTemplate(Patron, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed))
+                        {
+                            double[] minValues, maxValues;
+                            Point[] minLocations, maxLocations;
+                            resultado.MinMax(out minValues, out maxValues, out minLocations, out maxLocations);
+
+                            //valores del threshold 0.75 -- 0.9 es afirmativo
+
+                        if(maxValues[0] > 0.9)
+                            {
+                                // dibuja un rectangulo al rededor de  lo encontrado.
+
+                                Rectangle match = new Rectangle(maxLocations[0], Patron.Size);
+                                image.Draw(match, new Bgr(Color.Red), 3);
+                                pictureBox3.Image = image.ToBitmap();
+                                flag = true;
+                                pictureBox2.Image = Patron.ToBitmap();
+
+                                if (i % 2 == 0 )
+                                {
+                                    serialPort1.Write("run 2");
+                                    serialPort1.Write("\r");
+                                }
+                                else
+                                {
+                                    serialPort1.Write("run 3");
+                                    serialPort1.Write("\r");
+                                }
+                            }
+
+                        }
+
+                        if(flag == false)
+                        {
+                            serialPort1.Write("move 0");
+                            serialPort1.Write("\r");
+                        }
+                    }
+
+                }
+
+
+
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
